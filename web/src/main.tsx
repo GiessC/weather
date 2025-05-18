d4118ae063409b2aac815574bcd77f2db8650daa
Collin Giess <giesscollin@gmail.com>
2025-05-18 11:21:14 -0400

    add QueryClientProvider for react-query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './app/app.tsx'
+import { Provider } from './app/provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 );

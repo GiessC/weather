6447a786d9c9e72af742eeb15bb805b65d4d9f3d
Collin Giess <giesscollin@gmail.com>
2025-05-18 11:22:49 -0400

    add weather api for querying weather data and searching for locations

---
+++ web/src/features/weather/api/weather.api.ts
@@ -0,0 +1,38 @@
+import { api, apiUrl } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+import {
+  CurrentWeather,
+  type CurrentWeatherJson,
+} from '../types/current-weather';
+import type {
+  Coordinates,
+  Location,
+} from '@/features/location/hooks/useLocation';
+
+export function useWeatherApi() {
+  function useWeatherByLocation({ id, coords }: Location) {
+    return useQuery({
+      queryKey: ['weather', id, coords?.latitude, coords?.longitude],
+      queryFn: async (): Promise<CurrentWeather> => {
+        const endpoint = apiUrl('/current.json');
+        endpoint.searchParams.append(
+          'q',
+          id ? `id:${id}` : `${coords?.latitude},${coords?.longitude}`
+        );
+        const json = await api.get<CurrentWeatherJson>(endpoint);
+        return CurrentWeather.fromJson(json);
+      },
+    });
+  }
+
+  function useSearchLocation(query: string): Promise<CurrentWeatherJson> {
+    const endpoint = apiUrl('/current.json');
+    endpoint.searchParams.append('q', query);
+    return api.get<CurrentWeatherJson>(endpoint);
+  }
+
+  return {
+    useWeatherByLocation,
+    useSearchLocation,
+  };
+}

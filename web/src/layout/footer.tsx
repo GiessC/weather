af09d7b757b015e5761390bf410fc92b1360ccfb
Collin Giess <giesscollin@gmail.com>
2025-05-18 19:02:08 -0400

    add loading display for last updated at in footer

--- web/src/layout/footer.tsx
+++ web/src/layout/footer.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useWeather } from "@/features/weather/hooks/useWeather"
 import Constants from "@/utils/const";
 import { formatWithTime } from "@/utils/dayjs";
@@ -9,12 +10,12 @@ function goTo(url: string) {
 }
 
 export function Footer() {
-  const { weather } = useWeather();
+  const { weather, isLoading } = useWeather();
 
   return (
     <footer className='w-full fixed bottom-4 items-center'>
       <p className="text-sm text-muted-foreground text-center">
-        Last updated at {formatWithTime(weather?.lastUpdated)}
+        {isLoading ? <Skeleton className='m-auto w-[300px] h-[25px]' /> : <>Last updated at {formatWithTime(weather?.lastUpdated)}</>}
       </p>
       <p className="text-sm text-muted-foreground text-center">
         Brought to you by <Button variant='link' className='px-0' onClick={goTo(Constants.MY_URL)}>Collin Giess</Button> and the <Button className='px-0' variant='link' onClick={goTo(Constants.WEATHER_API_URL)}>Weather API</Button>

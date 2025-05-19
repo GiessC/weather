33a2f4973b9c0c765b808e9bc7a1038fc26f358d
Collin Giess <giesscollin@gmail.com>
2025-05-18 21:26:12 -0400

    change default to use kilometers

--- web/src/features/settings/types/settings.entity.ts
+++ web/src/features/settings/types/settings.entity.ts
@@ -2,10 +2,7 @@ import { z } from 'zod';
 
 export const settingsSchema = z.object({
   useFahrenheit: z.boolean().default(false),
-  useMiles: z.boolean().default(true),
+  useMiles: z.boolean().default(false),
 });
 export type Settings = z.infer<typeof settingsSchema>;
-export const DEFAULT_SETTINGS: Settings = {
-  useFahrenheit: false,
-  useMiles: true,
-};
+export const DEFAULT_SETTINGS: Settings = settingsSchema.parse({});
